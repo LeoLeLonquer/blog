f73eb8cbfdf741a691ebfd3fb2d6a1b176dba12f
LeoLeLonquer <leolouis.le-lonquer@laposte.net>
2022-10-05 00:19:21 +0200

    sidebar ajouté mais pas en position fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 var sectionHeight = function() {
   var total    = $(window).height(),
-      $section = $('section').css('height','auto');
+      $article = $('article').css('height','auto');
 
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
-  } else {
-    $section.css('height','auto');
-  }
+  // if ($article.outerHeight(true) < total) {
+  //   var margin = $article.outerHeight(true) - $article.height();
+  //   $article.height(total - margin - 20);
+  // } else {
+  //   $article.css('height','auto');
+  // }
 }
 
 $(window).resize(sectionHeight);
 
 $(function() {
-  $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+  $("article h1, article h2, article h3").each(function(){
+    $("nav.article ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
+    $("nav.article ul li:first-child a").parent().addClass("active");
   });
 
-  $("nav ul li").on("click", "a", function(event) {
+  $("nav.article ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
+    $("nav.article ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
   });
